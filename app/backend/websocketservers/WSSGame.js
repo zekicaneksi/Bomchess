8083fcf1acb02783e0942c675ea95c9d31d267c7
zeki can eksi <zekicaneksi@gmail.com>
2022-07-08 19:22:55 +0300

    timer is added to the both front end and backend. but its not very precise and delay is also needs to be calculated

--- app/backend/websocketservers/WSSGame.js
+++ app/backend/websocketservers/WSSGame.js
@@ -3,112 +3,144 @@ import { Chess } from 'chess.js'
 
 import {Games} from './Share.js';
 
-const WSSGame = new WebSocketServer({ noServer: true });
-const chess = new Chess();
-
-// Used as a response to 'ping'
-function heartbeat(){
-    this.isAlive = true;
-}
-
-function checkGameEnd(){
-    if(chess.game_over()){
-        if(chess.in_checkmate()){
-            let winner = "b";
-            if(chess.turn() == "b")
-                winner="w";
-            console.log('game has ended via checkmate, the winner is: ' + winner);
-        }else if(chess.in_draw()){
-            if(chess.in_stalemate()){
-                console.log('game has ended as a draw in stalemate');
-            }else if(chess.in_threefold_repetition()){
-                console.log('game has ended as a draw in threefold repetition');
-            }else if(chess.insufficient_material()){
-                console.log('game has ended as a draw in insufficient material');
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
-WSSGame.on('connection', (ws,req) => {
-
-    // JSON object to hold information about the game
-    let initials ={};
-
-    // Orientation
-    if(WSSGame.orientation.white == ws.user._id.toString()){
-        initials.orientation = "white";
-    }
-    else{
-        initials.orientation = "black";
-    }
-
-    // Current position of the board
-    initials.position = chess.fen();
-
-    // Send the initials
-    ws.send("initials:" + JSON.stringify(initials));
-
-    ws.on('message', (data) => {
-        
-        data = data.toString();
-        
-        let type = data.substring(0,data.indexOf(':'));
-        let content = data.substring(data.indexOf(':')+1);
-
-        if(type == 'play'){
-            // Check if the player has the turn to play
-            if(
-                (WSSGame.orientation.white == ws.user._id.toString() && chess.turn() == "w")
-                ||
-                (WSSGame.orientation.black == ws.user._id.toString() && chess.turn() == "b"))
-                {
-             
-                // Validate and then make the move
-                if(chess.move(JSON.parse(content)) != null){
-                    WSSGame.clients.forEach((webSocket) => {
-                        webSocket.send(data);
-                    });
-
-                    // Check if the game ended
-                    if(checkGameEnd()){
-                        
-                    }
-                }
-
-            }
-            
-        }
-    });
-
-    // Ping-pong messages to keep the connection active
-    ws.isAlive = true;
-    ws.on('pong', heartbeat);
-
-});
-
-// Ping-pong messages to detect and close broken connections
-const interval = setInterval( () => {
-
-    WSSGame.clients.forEach( (ws) => {
-    if (ws.isAlive === false) {
-        return ws.terminate();
-    }
-    ws.isAlive = false;
-    ws.ping();
-    });
-
-}, 10000);
-
-WSSGame.on('close', function close() {
-    clearInterval(interval);
-    Games.delete(WSSGame.players[0].id.toString(),WSSGame.players[1].id.toString());
-});
-
 function createWSSGame(){
+    const WSSGame = new WebSocketServer({ noServer: true });
+    const chess = new Chess();
+    
+    // Used as a response to 'ping'
+    function heartbeat(){
+        this.isAlive = true;
+    }
+    
+    // Checks if the game ended or not
+    function checkGameEnd(){
+        if(chess.game_over()){
+            if(chess.in_checkmate()){
+                let winner = "b";
+                if(chess.turn() == "b")
+                    winner="w";
+                console.log('game has ended via checkmate, the winner is: ' + winner);
+            }else if(chess.in_draw()){
+                if(chess.in_stalemate()){
+                    console.log('game has ended as a draw in stalemate');
+                }else if(chess.in_threefold_repetition()){
+                    console.log('game has ended as a draw in threefold repetition');
+                }else if(chess.insufficient_material()){
+                    console.log('game has ended as a draw in insufficient material');
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    // Timer for the game
+    let whiteRemainingTime=null;
+    let blackRemainingTime=null;
+    
+    const gameTimer = setInterval(() => {
+        if(chess.turn() == "b")
+            blackRemainingTime--;
+        else
+            whiteRemainingTime--;
+
+        if(whiteRemainingTime == 0 || blackRemainingTime == 0){
+            clearInterval(gameTimer);
+        }
+    },1000);
+    
+    WSSGame.on('connection', (ws,req) => {
+
+        // Set the remaining times
+        if(whiteRemainingTime == null){
+            whiteRemainingTime = WSSGame.matchLength*60;
+            blackRemainingTime = WSSGame.matchLength*60;
+        }
+            
+        // JSON object to hold information about the game
+        let initials ={};
+    
+        // Orientation
+        if(WSSGame.orientation.white == ws.user._id.toString()){
+            initials.orientation = "white";
+        }
+        else{
+            initials.orientation = "black";
+        }
+    
+        // Current position of the board
+        initials.position = chess.fen();
+
+        initials.whiteRemainingTime = whiteRemainingTime;
+        initials.blackRemainingTime = blackRemainingTime;
+
+        // Send the initials
+        ws.send("initials:" + JSON.stringify(initials));
+    
+        ws.on('message', (data) => {
+            
+            data = data.toString();
+            
+            let type = data.substring(0,data.indexOf(':'));
+            let content = data.substring(data.indexOf(':')+1);
+    
+            if(type == 'play'){
+                // Check if the player has the turn to play
+                if(
+                    (WSSGame.orientation.white == ws.user._id.toString() && chess.turn() == "w")
+                    ||
+                    (WSSGame.orientation.black == ws.user._id.toString() && chess.turn() == "b"))
+                    {
+                 
+                    // Validate and then make the move
+                    
+                    if(chess.move(JSON.parse(content)) != null){
+                        WSSGame.clients.forEach((webSocket) => {
+                            let toSend = {};
+                            toSend.move = JSON.parse(content);
+                            toSend.whiteRemainingTime = whiteRemainingTime;
+                            toSend.blackRemainingTime = blackRemainingTime;
+                            webSocket.send(type+':'+JSON.stringify(toSend));
+                        });
+    
+                        // Check if the game ended
+                        if(checkGameEnd()){
+                            
+                        }
+                    }
+    
+                }
+                
+            }
+        });
+    
+        // Ping-pong messages to keep the connection active
+        ws.isAlive = true;
+        ws.on('pong', heartbeat);
+    
+    });
+    
+    // Ping-pong messages to detect and close broken connections
+    const pingPongInterval = setInterval( () => {
+    
+        WSSGame.clients.forEach( (ws) => {
+        if (ws.isAlive === false) {
+            return ws.terminate();
+        }
+        ws.isAlive = false;
+        ws.ping();
+        });
+    
+    }, 10000);
+    
+    WSSGame.on('close', function close() {
+        clearInterval(pingPongInterval);
+        clearInterval(gameTimer);
+        Games.delete(WSSGame.players[0].id.toString(),WSSGame.players[1].id.toString());
+    });
+
     return WSSGame;
 }
+
 
 export {createWSSGame};
--- app/front_end/src/pages/Game.js
+++ app/front_end/src/pages/Game.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState, useRef} from 'react';
 import * as HelperFunctions from '../components/HelperFunctions';
 import {Chess} from "chess.js";
 import { Chessboard } from "react-chessboard";
+import './Game.css';
 
 const Game = () => {
 
@@ -11,7 +12,12 @@ const Game = () => {
   const socket = useRef();
   const initials = useRef({});
 
-  // Function for move validation
+  const [blackRemainingTime, setBlackRemainingTime] = useState(0);
+  const [whiteRemainingTime, setWhiteRemainingTime] = useState(0);
+  const turn = useRef();
+  const timer = useRef();
+
+  // Function to update the board
   function safeGameMutate(modify) {
     setGame((g) => {
       const update = { ...g };
@@ -20,6 +26,7 @@ const Game = () => {
     });
   }
 
+  // Function to check if the game is ended or not
   function checkGameEnd(){
     if(game.game_over()){
       if(game.in_checkmate()){
@@ -42,25 +49,29 @@ const Game = () => {
   }
 
   function onDrop(sourceSquare, targetSquare) {
-    // Validate the move
-    let move = null;
-    safeGameMutate((game) => {
-      move = game.move({
-        from: sourceSquare,
-        to: targetSquare,
-        promotion: "q", // always promote to a queen for example simplicity
-      });
+
+    // Make the move
+    let move = game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
     });
 
     // Move is illegal
-    if (move === null) return false;
+    if(move === null)  return false;
 
-    // Move is legal
+    // Move is legal, update the board
+    safeGameMutate((game) => {
+      game.move(move);
+    });
+
+    // Send the move to the server
     socket.current.send("play:"+JSON.stringify(move));
     
     return true;
   }
 
+  // Makes opponent's pieces undraggable
   function isDraggablePiece({piece, sourceSquare}){
 
     if(piece[0] != initials.current.orientation[0]){
@@ -74,48 +85,61 @@ const Game = () => {
   useEffect(() => {
 
     // --- ComponentDidMount
+
+    // Create the WebSocket
     socket.current = new WebSocket('ws://localhost:'+ HelperFunctions.apiPort + '/api/game');
-    
-    // Connection opened
-    socket.current.addEventListener('open', function (event) {
-        
-    });
     
     // Listen for messages
     socket.current.addEventListener('message', function (event) {
+
+      // Parse the message
       let type = event.data.substring(0,event.data.indexOf(':'));
       let content = event.data.substring(event.data.indexOf(':')+1);
 
       if(type == 'play'){
+
         content = JSON.parse(content);
+
         safeGameMutate((game) => {
-            game.move({
-            from: content.from,
-            to: content.to,
-            promotion: content.promotion, // always promote to a queen for example simplicity
-          });
+            game.move(content.move);
         });
+
+        setBlackRemainingTime(content.blackRemainingTime);
+        setWhiteRemainingTime(content.whiteRemainingTime);
+        turn.current = (turn.current=='w' ? 'b' : 'w');
 
         // Check if the game ended
         checkGameEnd();
 
+        // initials is sent when connected to setup the game
       } else if (type=="initials"){
         initials.current = JSON.parse(content);
+        setBlackRemainingTime(initials.current.blackRemainingTime);
+        setWhiteRemainingTime(initials.current.whiteRemainingTime);
         game.load(initials.current.position);
+        turn.current=game.turn();
         setLoadBoard(true);
+
+        // Start the countdown timer for the remaining times
+        timer.current = setInterval(() => {
+          if(turn.current == 'b')
+            setBlackRemainingTime((oldTime) => oldTime-1);
+          else
+            setWhiteRemainingTime((oldTime) => oldTime-1);
+        },1000);
       }
 
     });
 
-    // Connection opened
+    // Connection failed
     socket.current.addEventListener('error', function (event) {
-      // in here, i need to make chat box passive like unaccessible looking for the user
       alert("can't connect");
     });
 
     // --- ComponentWillUnmount
     return () => {
       socket.current.close();
+      clearInterval(timer.current);
     }
   }, []);
 
@@ -123,7 +147,12 @@ const Game = () => {
     return(<p>Loading...</p>);
   }
   else{
-    return(<Chessboard position={game.fen()} onPieceDrop={onDrop} boardOrientation={initials.current.orientation} isDraggablePiece={isDraggablePiece}/>);
+    return(
+    <div>
+      <Chessboard position={game.fen()} onPieceDrop={onDrop} boardOrientation={initials.current.orientation} isDraggablePiece={isDraggablePiece}/>
+      <p className='game-timer'>{(initials.current.orientation == "white") ? String(blackRemainingTime) : String(whiteRemainingTime)}</p>
+      <p className='game-timer'>{(initials.current.orientation == "white") ? String(whiteRemainingTime) : String(blackRemainingTime)}</p>
+    </div>);
   }
 
 };
