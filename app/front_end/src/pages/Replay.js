aad5620891f0524dbb8f872429d868a63ec4d9cc
zeki can eksi <zekicaneksi@gmail.com>
2022-08-03 21:51:14 +0300

    next,previous and switch side buttons are working

--- app/front_end/src/pages/Replay.js
+++ app/front_end/src/pages/Replay.js
@@ -16,26 +16,42 @@ const Replay = (props) => {
     const [game, setGame] = useState(new Chess());
     const [matchInfo, setMatchInfo] = useState();
     const [remainingTimes, setRemainingTimes] = useState({white: 0, black: 0});
+    const [moveNumber, setMoveNumber] = useState();
+    const [orientation, setOrientation] = useState('w');
+
+    useEffect(() => {
+        if(moveNumber!==undefined){
+            let whiteRemainingTime, blackRemainingTime;
+            if(moveNumber % 2 === 0){
+                let whiteMove = matchInfo.fenArrayWithRemainingTimes.whiteMoves[moveNumber/2];
+                whiteRemainingTime = whiteMove.remainingTime;
+                blackRemainingTime = (moveNumber === 0 ? remainingTimes.black : matchInfo.fenArrayWithRemainingTimes.blackMoves[(moveNumber/2)-1].remainingTime);
+                game.load(whiteMove.fen);
+            } else {
+                let blackMove = matchInfo.fenArrayWithRemainingTimes.blackMoves[parseInt(moveNumber/2)];
+                blackRemainingTime = blackMove.remainingTime;
+                whiteRemainingTime = matchInfo.fenArrayWithRemainingTimes.whiteMoves[(moveNumber-1)/2].remainingTime;
+                game.load(blackMove.fen);
+            }
+            setRemainingTimes({
+                white: whiteRemainingTime,
+                black: blackRemainingTime
+            });
+        }
+    }, [moveNumber]);
 
 
     function movesListOnClickHandle(moveNumber){
-        let whiteRemainingTime, blackRemainingTime;
-        if(moveNumber % 2 === 0){
-            let whiteMove = matchInfo.fenArrayWithRemainingTimes.whiteMoves[moveNumber/2];
-            whiteRemainingTime = whiteMove.remainingTime;
-            blackRemainingTime = (moveNumber === 0 ? remainingTimes.black : matchInfo.fenArrayWithRemainingTimes.blackMoves[(moveNumber/2)-1].remainingTime);
-            game.load(whiteMove.fen);
-        } else {
-            let blackMove = matchInfo.fenArrayWithRemainingTimes.blackMoves[parseInt(moveNumber/2)];
-            blackRemainingTime = blackMove.remainingTime;
-            whiteRemainingTime = matchInfo.fenArrayWithRemainingTimes.whiteMoves[(moveNumber-1)/2].remainingTime;
-            game.load(blackMove.fen);
-        }
-        setRemainingTimes({
-            white: whiteRemainingTime,
-            black: blackRemainingTime
-        });
+        setMoveNumber(moveNumber);
+    }
 
+    function nextBtnHandle(){
+        if(moveNumber === undefined) setMoveNumber(0)
+        else if(moveNumber!==matchInfo.moves.length-1) setMoveNumber(old => old+1);
+    }
+
+    function previousBtnHandle(){
+        if(moveNumber!==0 && moveNumber !== undefined) setMoveNumber(old => old-1);
     }
 
     function crateFenForMovesWithRemainingTimes(moves, gameLength, gameDate){
@@ -150,15 +166,15 @@ const Replay = (props) => {
                     moves={matchInfo.moves}
                     orientation={'v'}
                     onClickHandle={movesListOnClickHandle}/>
-                    <button>Next</button>
-                    <button>Previous</button>
-                    <button>switch side</button>
+                    <button onClick={previousBtnHandle}>Previous</button>
+                    <button onClick={nextBtnHandle}>Next</button>
+                    <button onClick={() => {setOrientation(old => (old === 'w' ? 'b' : 'w'))}}>switch side</button>
                 </div>
                 <div className='replay-middle-div-container'>
                     <GameBoard
                     game={game}
                     setGame={setGame}
-                    myColor='w'
+                    myColor={orientation}
                     arePiecesDraggable={false}
                     disableSquareClick={true}
                     />
