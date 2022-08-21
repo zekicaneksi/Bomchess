6f48eaa4570802f75806ff44d82a2bd601855c41
zeki can eksi <zekicaneksi@gmail.com>
2022-08-21 15:32:01 +0300

    fixed bot

--- app/backend/BomchessBot.js
+++ app/backend/BomchessBot.js
@@ -3,6 +3,7 @@ import {WSSLobby} from './websocketservers/WSSLobby.js';
 import {User} from "./model/user.js";
 import {Message} from "./model/message.js"
 import { newMessage } from './websocketservers/WSSLayout.js';
+import bcrypt from "bcryptjs";
 
 const { API_PORT } = process.env;
 
@@ -10,6 +11,20 @@ const email = "bot@bomchess.com";
 const username = "BomchessBot";
 
 const BomchessBot = async () => {
+
+    // If the bot is not created, create it
+
+    const testBot = await User.findOne({"email" : email});
+    const password = (Math.floor(Math.random() * 1000) + 1).toString(); // a random number between a thousand and one
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    if(!testBot) {
+        await User.create({
+            username: "BomchessBot", 
+            email: "bot@bomchess.com".toLowerCase(),
+            password: encryptedPassword,
+            bio: "Hello, i am BomchessBot. Send me a message and i will send it back to you in reverse in about 5 seconds."
+        });
+    }
 
     let socket = new WebSocket('ws://localhost:'+ API_PORT + '/api/layout');
 
