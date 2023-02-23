9aae6ba30ceacc89185ae2643420cee4449d23e1
zeki can eksi <zekicaneksi@gmail.com>
2023-02-23 10:48:03 +0300

    removed env files and added to gitignore

--- app/_build/index.js
+++ app/_build/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const path = require('path');
-var proxy = require('express-http-proxy');
 require('dotenv').config()
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', proxy('localhost:'+process.env.BACKEND_PORT, {
-    proxyReqPathResolver: function (req) {
-	return '/api'+ req.url;}
-    }));
 
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'public', 'index.html'));
